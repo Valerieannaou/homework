0336f371d990b041efd6122ea13eb23dff2dfd90
vagrant <vagrant@kellogg-dev-box>
2014-04-22 03:12:39 +0000

    Books in a Table

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,2 +1,9 @@
 class DiceController < ApplicationController
+  def show
+    @images = {"http://www.kiei925.com/images/dice/1.png", "http://www.kiei925.com/images/dice/2.png", "http://www.kiei925.com/images/dice/3.png", "http://www.kiei925.com/images/dice/4.png", "http://www.kiei925.com/images/dice/5.png", "http://www.kiei925.com/images/dice/6.png"]
+      @random_no = rand(6)
+      @random_image = @images[@random_no]
+    end
+
+
 end
